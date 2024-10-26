4beafb363867a7a3847145cd8359929381d28a11
FedericoLazarte <fedelazarte2015@gmail.com>
2024-10-26 10:03:44 -0300

    arreglando detalles

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -6,51 +6,38 @@ import logica.Oferta;
 import logica.SalaDeEnsayo;
 import javax.swing.JOptionPane;
 
-
 public class Controlador {
 	private SalaDeEnsayo sala;
-    
-    public Controlador() {
-        this.sala = new SalaDeEnsayo();
-    }
-    
-    public void crearOferta(Oferta oferta) {
-    	cargarOfertas(oferta);
-    }
-    
-    public void cargarOfertas(Oferta oferta) {
-    	this.sala.registrarOferta(oferta);
-    }
-   
-    public void iniciarOfertas() {
-	    List<Oferta> ofertasOptimas = sala.encontrarOfertasOptimas();
-	    if (!ofertasOptimas.isEmpty()) {
-	        int gananciaTotal = sala.calcularGananciaTotal(ofertasOptimas);
-	        StringBuilder sb = new StringBuilder();
-	        sb.append("Ofertas adjudicadas:\n");
-	        for (Oferta oferta : ofertasOptimas) {
-	            sb.append(oferta).append("\n");
-	        }
-	        sb.append("Ganancia total: $").append(gananciaTotal);
-	        JOptionPane.showMessageDialog(null, sb.toString());  // Mostrar cuadro de diálogo
-	    } else {
-	        JOptionPane.showMessageDialog(null, "No se encontraron ofertas para la sala por el momento");
-	    }
-	}
-    
-    
-    
-    public List<String> obtenerOfertasComoTexto() {
-        return sala.obtenerOfertasComoTexto();
-    }
 
-	public void ofertasPredeterminadas() {
-		sala.ofertasPredeterminadas();
+	public Controlador() {
+		this.sala = new SalaDeEnsayo();
 	}
 
-	
+	public void crearOferta(Oferta oferta) {
+		cargarOfertas(oferta);
+	}
 
-	
+	public void cargarOfertas(Oferta oferta) {
+		this.sala.registrarOferta(oferta);
+	}
 
-	
+	public void iniciarOfertas() {
+		List<Oferta> ofertasOptimas = sala.encontrarOfertasOptimas();
+		if (!ofertasOptimas.isEmpty()) {
+			double gananciaTotal = sala.calcularGananciaTotal(ofertasOptimas);
+			StringBuilder sb = new StringBuilder();
+			sb.append("Ofertas adjudicadas:\n");
+			for (Oferta oferta : ofertasOptimas) {
+				sb.append(oferta).append("\n");
+			}
+			sb.append("Ganancia total: $").append(gananciaTotal);
+			JOptionPane.showMessageDialog(null, sb.toString()); // Mostrar cuadro de diálogo
+		} else {
+			JOptionPane.showMessageDialog(null, "No se encontraron ofertas para la sala por el momento");
+		}
+	}
+
+	public List<String> obtenerOfertasComoTexto() {
+		return sala.obtenerOfertasComoTexto();
+	}
 }
